25468d65750af235f69f1e5334cdbe2ae8748869
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-20 23:40:17 -0400

    Use Clinton's django template to start project

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Consider creating a custom user model from scratch as detailed at
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#specifying-a-custom-user-model
+
+
+class User(AbstractUser):
+    pass
