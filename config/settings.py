85e86a33b375e61003012a274fa3abed91e0d5e3
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-21 12:22:52 -0400

    Update settings to read DEBUG from environment

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
