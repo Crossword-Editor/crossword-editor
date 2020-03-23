ef085b03ba4736d8bcacde9bef51f69403adf4ac
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-23 12:47:17 -0400

    Remove User imports in editor admin, add Puzzle

--- editor/admin.py
+++ editor/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
 
+from .models import Puzzle
 
-# from .models import ModelName
-# admin.site.register(ModelName)
-
-admin.site.register(User, UserAdmin)
+admin.site.register(Puzzle)
