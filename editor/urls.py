425157c4ba816b191271b57eab49978b4c4cf315
cheryl <rook2q5@gmail.com>
2020-03-23 17:19:20 -0400

    final startup branch

--- editor/urls.py
+++ editor/urls.py
@@ -1,9 +1,8 @@
 from django.urls import include, path
 from . import views
 
-
 urlpatterns = [
-    # path('', views.home, name="home"),
+    path('', views.home, name="home"),
     path('', views.welcome, name="welcome"),
     path('', views.user_home, name="user_home"),
 ]
