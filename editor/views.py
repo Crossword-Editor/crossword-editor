5c3c4d1c0a2d547b3fdaaef269f2ca36a3f3a62c
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-25 12:00:00 -0400

    Remove unnecessary import

--- editor/views.py
+++ editor/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .models import Puzzle
